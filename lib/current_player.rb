43b181812271cefff423f875a7bbf3fcf3ae9270
Christian V <viray.christianj@gmail.com>
2018-08-08 00:54:50 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,11 @@
 def turn_count(board)
 counter = 0
 board.each do |turn|
-  if turn 
+  if turn == "X" || turn == "O"
   counter += 1
+end 
+return counter
+end
 end 
 
 def current_player(turn_count)
