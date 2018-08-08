33edc49980ff6f6155b0ea4248c3ac14e910425f
Christian V <viray.christianj@gmail.com>
2018-08-08 00:53:04 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+board = [" "," "," "," "," "," "," "," "," "]
+
+
+def turn_count(board)
+turn = 0
+if board.each do |turn|
+  counter += 1
+end 
+
+def current_player(turn_count)
+  if turn_count(board).even
+    return "X"
+  else 
+    return "O"
+  end
+end 
